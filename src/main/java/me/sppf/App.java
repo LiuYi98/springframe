9bee37a096a9a02bb2ea1544d8ff674b8626c3b8
liuyi <310448103@qq.com>
2023-02-26 14:08:51 +0800

    add

--- src/main/java/me/sppf/App.java
+++ src/main/java/me/sppf/App.java
@@ -1,13 +1,14 @@
 package me.sppf;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("");
     }
 }
